68354ffbfcbe8c2937ef93e0c431b7713ab17ee9
phe0 <pheofo@gmail.com>
2020-01-28 13:35:51 -0300

    improve bubble nav

--- src/components/BubbleNav/BubbleNav.js
+++ src/components/BubbleNav/BubbleNav.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './BubbleNav.scss'
 
 class BubbleNav extends React.Component {
+
     state = {
         isOpen: false,
         position1: false,
@@ -12,28 +13,47 @@ class BubbleNav extends React.Component {
 
     render() {
         return (
-            <div className={`bubble-nav ${this.state.isOpen ? 'front-enable': ''} `}>
+            <div className={`bubble-nav ${this.state.isOpen ? 'front-enable' : ''} `}>
                 <div className={`modal-${this.state.isOpen ? 'open' : ''}`} onClick={() => this.closeBubble()}>
-
                 </div>
                 <div className={`bubble-nav__bubble position-1${this.state.position1 ? '__open' : ''}`} onClick={() => this.openBubble(1)}>
-    
+                    {
+                        this.state.position1 ? 
+                            null 
+                            : 
+                            <h6 className="bubble-nav__bubble__item1">ABOUT ME</h6>
+                    }
                 </div>
                 <div className={`bubble-nav__bubble position-2${this.state.position2 ? '__open' : ''}`} onClick={() => this.openBubble(2)}>
-    
+                    {
+                        this.state.position2 ? 
+                            null 
+                            : 
+                            <h6 className="bubble-nav__bubble__item2">MY SKILLS</h6>
+                    }
                 </div>
                 <div className={`bubble-nav__bubble position-3${this.state.position3 ? '__open' : ''}`} onClick={() => this.openBubble(3)}>
-    
+                    {
+                        this.state.position3 ? 
+                        null 
+                        : 
+                        <h6 className="bubble-nav__bubble__item3">EXPERIENCES</h6>
+                    }
                 </div>
                 <div className={`bubble-nav__bubble position-4${this.state.position4 ? '__open' : ''}`} onClick={() => this.openBubble(4)}>
-    
+                    {
+                        this.state.position4 ? 
+                        null 
+                        : 
+                        <h6 className="bubble-nav__bubble__item4">CONTACT ME</h6>
+                    }
                 </div>
             </div>
         )
     }
 
     openBubble = (position) => {
-        const place = 'position'+ position
+        const place = 'position' + position
         this.setState({
             position1: false,
             position2: false,
